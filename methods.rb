c9af54353a8e14f7759614780cf8affc35edda43
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-10-17 23:38:34 +0300

    lab complete

--- methods.rb
+++ methods.rb
@@ -1,1 +1,50 @@
 # Your code here!
+
+# function myFunction(param) {
+#   console.log("Running myFunction");
+#   return param + 1;
+#   const myFunctionReturnValue = myFunction(1);
+#   console.log(myFunctionReturnValue);
+
+# }
+# def my_method(param)
+#     puts "Running my_method"
+#     param + 1
+
+# end
+
+def greet_programmer
+
+    puts "Hello, programmer!"
+
+end    
+
+def greet(name)
+
+    puts "Hello, #{name}!"
+
+end
+
+def greet_with_default(name = "programmer")
+
+    puts "Hello, #{name}!"
+
+end
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+def halve(num1)
+    
+    if num1.class != Integer
+        return nil
+    end
+
+    return num1 / 2
+
+end
+
+
+
+
